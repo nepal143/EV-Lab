62967b0ef5c474df7efcbbe8f96917013d689150
Nepal Singh <71310255+nepal143@users.noreply.github.com>
2023-10-16 21:28:51 +0530

    updated  ev lab

--- script.js
+++ script.js
@@ -1,35 +1,60 @@
-window.addEventListener("scroll" , ()=>{
-    let height  = window.innerHeight ;
-    let scroll_height  = window.scrollY ;
-    console.log(scroll_height)
-    if(scroll_height >= 100){
+window.addEventListener("scroll", () => {
+    let height = window.innerHeight;
+    let scroll_height = window.scrollY;
+    if (scroll_height >= 100) {
         let opacity = document.getElementsByClassName("zero-opacity");
-        for(var i = 0 ;i < opacity.length ;i++) {
+        for (var i = 0; i < opacity.length; i++) {
             opacity[i].classList.add("one-opacity");
             opacity[i].classList.remove("zero-opacity");
         };
-    }
-    else{
+    } else {
         let opacity = document.getElementsByClassName("one-opacity");
-        for(var i = 0 ;i < opacity.length ;i++) {
+        for (var i = 0; i < opacity.length; i++) {
             opacity[i].classList.add("zero-opacity");
             opacity[i].classList.remove("one-opacity");
         };
-        
     }
-})
+});
+
 let main_card_number = 1
 
+let cards = document.getElementsByClassName("card");
 
-const show_cards = ()=>{
-    let cards = document.getElementsByClassName("card");
-    for(var i = 0 ; i < cards.length ; i++){
-        cards[i].style.display = "none" ;
-    }
-    for(var i = main_card_number-1  ; i < main_card_number+2 ; i++){
-        cards[i].style.display = "flex" ;
+const show_cards = () => {
+    for (var i = 0; i < cards.length; i++) {
+        if (i != main_card_number - 1 && i != main_card_number + 1 && i != main_card_number)
+            cards[i].style.display = "none";
     }
     cards[main_card_number].classList.add("card-active");
+}
+
+const card_decrement = () => {
+    if (main_card_number + 1 <= cards.length) {
+        cards[main_card_number].classList.remove("card-active");
+        cards[main_card_number + 1].classList.add("card-active");
+        
+        if (main_card_number + 2 <= cards.length - 1) {
+            cards[main_card_number + 2].style.display = "flex";
+        }
+        if (main_card_number - 1 >= 0) {
+            cards[main_card_number - 1].style.display = "none";
+        }
+        main_card_number++;
+    }
+}
+
+const card_increment = () => {
+    if (main_card_number - 1 >= 0) {
+        cards[main_card_number].classList.remove("card-active");
+        cards[main_card_number - 1].classList.add("card-active");
+        if (main_card_number - 2 >= 0) {
+            cards[main_card_number - 2].style.display = "flex";
+        }
+        if (main_card_number + 1 < cards.length) {
+            cards[main_card_number + 1].style.display = "none";
+        }
+        main_card_number--;
+    }
 }
 
 show_cards();
