552940dcd5d6dcfef25911ce5a278994a3ab6576
Nepal Singh <nepalsss007@gmail.com>
2023-12-21 02:28:46 +0530

    added nav bar

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
 let cros = document.getElementsByClassName("toggle1")[0] ; 
 
-let nav = document.getElementsByClassName("nav-links"); 
+let nav = document.getElementsByTagName("nav")[0]; 
 
 let toggle = ()=>{
     if("fa-x" == cros.classList[2]){
         cros.classList.remove("fa-x") ;
         cros.classList.add("fa-bars") ; 
         console.log(cros.classList)
+        nav.classList.add("width0") ; 
     }
     else{
-
+        
         cros.classList.add("fa-x") ;
         cros.classList.remove("fa-bars") ; 
         console.log(cros.classList) ;   
+        nav.classList.remove("width0") ;
+
     }
 }
